fe7dd068946679d0cf5a2faf5a5241b1649ad96d
namsanimallesh0711 <namsanimallesh0711@gmail.com>
2020-04-03 23:12:51 +0530

    Login api changes committed

--- AnbDemoforPOC/AnbDemoforPOC/Controllers/LoginController.cs
+++ AnbDemoforPOC/AnbDemoforPOC/Controllers/LoginController.cs
@@ -6,6 +6,7 @@ using AnbDemoforPOC.Models;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sql;
 
 namespace AnbDemoforPOC.Controllerss
 {
--- AnbDemoforPOC/AnbDemoforPOC/Startup.cs
+++ AnbDemoforPOC/AnbDemoforPOC/Startup.cs
@@ -25,13 +25,13 @@ namespace AnbDemoforPOC
         {
             Configuration = configuration;
         }
-        
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           
+
 
             services.AddIdentity<AppUser, AppRole>(options =>
             {
